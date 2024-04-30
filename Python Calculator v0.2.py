4cd786b0cf2a72f6c801e1a79ebdeeb0cc6a3cd4
CH3Hg <168161455+CH3Hg@users.noreply.github.com>
2024-04-30 20:11:49 +0800

    Add files via upload
    repair the mistake

---
+++ Python Calculator v0.2.py
@@ -0,0 +1,21 @@
+print('==========Python Calculator v0.1==========')
+print('by CH3Hg')
+while True:
+    try:
+        num1=float(input('Please enter the first number:'))
+        calculation_symbol=input('Please enter the calculation symbol(+ - * /):')
+        num2=float(input('Please enter the second number:'))
+        if calculation_symbol=='+':
+            answer=num1+num2
+            print('Answer:',answer)
+        if calculation_symbol=='-':
+            answer=num1-num2
+            print('Answer:',answer)
+        if calculation_symbol=='*':
+            answer=num1*num2
+            print('Answer:',answer)
+        if calculation_symbol=='/':
+            answer=num1/num2
+            print('Answer:',answer)
+    except:
+            print('ERROR!(stop:0001)')
